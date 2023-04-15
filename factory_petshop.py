d1549d04dbf3e25230f1689fb676335a948856ed
FyFT0000 <facundoft@gmail.com>
2023-04-15 18:27:49 +0200

    Factory pattern

--- factory_petshop.py
+++ factory_petshop.py
@@ -11,3 +11,25 @@ class Dog:
 
     def speak(self):
         return 'Woof!'
+    
+class Cat:
+    """ A simple cat class """
+    
+    def __init__(self,name):
+        self.name = name
+
+    def speak(self):
+        return 'Meow!'
+
+
+def get_pet(pet = 'dog'):
+    """ The Factory Method """
+    pets = dict(dog = Dog('Hope'), cat = Cat('Peace'))
+    return pets[pet]
+    
+
+d = get_pet('dog')
+print(d.speak())
+
+c = get_pet('cat')
+print(c.speak())
